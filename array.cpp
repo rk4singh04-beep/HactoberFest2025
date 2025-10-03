dc988ae11153c18504fde6feb7cfb1f6c5ab2fff
rk4singh04-beep <rk4singh04@gmail.com>
2025-10-03 19:34:28 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,8 @@
+// Declare an array of 5 integers
+int numbers[5]; 
+
+// Declare and initialize an array of 4 strings
+std::string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+
+// Declare and initialize an array, letting the compiler determine the size
+int scores[] = {85, 92, 78}; // scores will have a size of 3
